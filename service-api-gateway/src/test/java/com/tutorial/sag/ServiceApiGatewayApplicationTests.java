79aacb4a500e8f47139f62787c9d3fa0d13b28f8
saurabh <kumarsaurabh23@gmail.com>
2021-09-23 22:20:54 -0500

    adding api-gateway and service-registry modules

---
+++ service-api-gateway/src/test/java/com/tutorial/sag/ServiceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.sag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
